d62ec0747505251abe7d53507f5138c77adaa1a0
Carlos15157 <53578256+Carlos15157@users.noreply.github.com>
2020-04-07 23:04:00 -0300

    Projeto das placas utilizando javascript

---
+++ script1.js
@@ -0,0 +1,23 @@
+function placa_carro(){
+    var placa = prompt("Digite a placa completa do seu carro.", "Exemplo(NNN-0000)");
+    console.log(placa_carro);
+  
+    if (placa.length < 8 || placa.length > 8) {
+      alert(
+        "Digite os números \n Exemplo : DVD-2020"
+      );
+    } else {
+      var circular = placa.substring(7, 8);
+  
+      if (circular == 1 || circular == 2)
+        alert("Você não pode circular as Segundas-Feiras");
+  
+      if (circular == 3 || circular == 4) alert("Você não pode circular as Terças-Feiras");
+      if (circular == 5 || circular == 6)
+        alert("Você não pode circular as Quartas-Feiras");
+      if (circular == 7 || circular == 8)
+        alert("Você não pode circular as Quintas-Feiras");
+      if (circular == 9 || circular == 0) 
+      alert("Você não pode circular as Sexta-Feiras");
+    }
+  }
